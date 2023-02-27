a951bbdcc7d32012a4c2b305709f53b0776deff5
Muniox <muniox20@gmail.com>
2023-02-27 22:57:50 +0100

    Added post category to database structure sql.

--- js_dose.sql
+++ js_dose.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Wersja serwera:               10.11.2-MariaDB - mariadb.org binary distribution
+-- Wersja serwera:               10.6.9-MariaDB - mariadb.org binary distribution
 -- Serwer OS:                    Win64
 -- HeidiSQL Wersja:              11.3.0.6295
 -- --------------------------------------------------------
@@ -19,34 +19,31 @@ USE `js_dose`;
 
 -- Zrzut struktury tabela js_dose.posts
 CREATE TABLE IF NOT EXISTS `posts` (
-  `id` varchar(36) NOT NULL DEFAULT uuid(),
-  `title` varchar(255) NOT NULL,
-  `desc` mediumtext NOT NULL,
-  `img` varchar(255) NOT NULL,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT uuid(),
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `desc` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` datetime NOT NULL DEFAULT curdate(),
-  `user_id` varchar(36) NOT NULL,
+  `user_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_posts_users` (`user_id`),
   CONSTRAINT `FK_posts_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Zrzucanie danych dla tabeli js_dose.posts: ~0 rows (około)
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+-- Eksport danych został odznaczony.
 
 -- Zrzut struktury tabela js_dose.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` varchar(36) NOT NULL DEFAULT uuid(),
-  `name` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `img` varchar(255) DEFAULT NULL,
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT uuid(),
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Zrzucanie danych dla tabeli js_dose.users: ~0 rows (około)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+-- Eksport danych został odznaczony.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
