1e06c241a1d26ce31519d2bb938ca09d8ad9d228
Muniox <muniox20@gmail.com>
2023-02-28 19:51:28 +0100

    Fixed wrong path for diskStorage multer.

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,8 +1,9 @@
 import { diskStorage } from 'multer';
 
+// @TODO Change upload directory for production
 export const storage = diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../client/public/upload');
+    cb(null, '../blog-megak-front/public/upload');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
