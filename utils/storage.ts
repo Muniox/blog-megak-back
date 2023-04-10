f38c2069dc09bb327bd7a76f4a5e48bf5cba6dfd
Muniox <muniox20@gmail.com>
2023-04-10 22:03:25 +0200

    Deleted todo.

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,7 +1,6 @@
 import config from '../global/config';
 import { diskStorage } from 'multer';
 
-// @TODO Zmienić ścieżkę dla folderu upload, żeby była jedna produkcyjna i druga developerska
 export const storage = diskStorage({
   destination: (req, file, cb) => {
     if (config.NODE_ENV === 'development') {
